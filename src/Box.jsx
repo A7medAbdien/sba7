950cdfaa0ce1cdb0d388948eaab368f1c764e687
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-02-16 08:51:45 +0300

    apply scroll

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,14 +1,20 @@
 import { useScroll } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber'
-export default function Box(props) {
-    const scroll = useScroll()
-    console.log(scroll);
-    useFrame((state, delta) => {
-        // const offset = 1 - scroll.offset
 
+
+export default function Box(props) {
+
+    const scroll = useScroll()
+
+    console.log(scroll)
+
+    useFrame((state, delta) => {
+        const offset = 1 - scroll.offset
+        state.camera.position.y = - scroll.offset * 16
+        // state.camera.lookAt(0, 0, 0)
     })
     return <mesh {...props}>
-        <boxGeometry args={[2, 2.5, 1]} />
-        <meshNormalMaterial />
+        <boxGeometry args={[2, 2, 1]} />
+        <meshNormalMaterial wireframe />
     </mesh>
 };
--- src/Experience.jsx
+++ src/Experience.jsx
@@ -5,10 +5,15 @@ import Box from './Box'
 export default function Experience() {
     return <>
         {/* <OrbitControls makeDefault /> */}
-        <axesHelper args={[2, 2, 2]} />
-        <ScrollControls>
+        <axesHelper args={[-2, 2, 2]} />
+
+
+        <ScrollControls pages={2}>
             <Box />
-            <Box position={[0, -3, 0]} />
+            <Box position={[0, -4, 0]} />
+            <Box position={[0, -8, 0]} />
+            <Box position={[0, -12, 0]} />
+            <Box position={[0, -16, 0]} />
         </ScrollControls>
     </>
 }
