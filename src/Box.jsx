8d5fbb85e1dddd893b5ee8f862f441f49c6f43e2
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-02-16 10:22:38 +0300

    apply rotation

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,20 +1,25 @@
 import { useScroll } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber'
+import { useRef } from 'react'
+import * as THREE from 'three';
 
 
 export default function Box(props) {
 
     const scroll = useScroll()
+    const mesh = useRef()
 
-    console.log(scroll)
-
+    // console.log(scroll)
     useFrame((state, delta) => {
-        const offset = 1 - scroll.offset
-        state.camera.position.y = - scroll.offset * 16
+        // const offset = 1 - scroll.offset
+        // state.camera.position.y = - scroll.offset * 16
+        mesh.current.rotateOnAxis(new THREE.Vector3(-1, 1, 0), delta)
         // state.camera.lookAt(0, 0, 0)
     })
-    return <mesh {...props}>
-        <boxGeometry args={[2, 2, 1]} />
-        <meshNormalMaterial wireframe />
-    </mesh>
+    return <>
+        <mesh ref={mesh} {...props}>
+            <boxGeometry args={[2, 2.5, 1]} />
+            <meshNormalMaterial />
+        </mesh>
+    </>
 };
