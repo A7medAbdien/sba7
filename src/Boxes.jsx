c9228cca7b7507265d4d30ed46638278dc124311
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-02-24 22:45:47 +0300

    rename

--- src/Boxes.jsx
+++ src/Boxes.jsx
@@ -2,48 +2,49 @@ import { Float, useHelper } from '@react-three/drei';
 import { useControls } from 'leva';
 import { useEffect, useRef, useState } from 'react';
 import { SpotLightHelper } from 'three';
-// import Box from "./Box"
-// import { getCoordinates } from "./Box"
 
-const Box = (props) => {
+const getCoordinates = (angle, distance = 6) => {
+    angle *= Math.PI / 180
+    let x = -distance * Math.cos(angle) + 1.75,
+        y = -distance * Math.sin(angle)
 
-    let { trigger, color } = props
-    const [theta, setTheta] = useState(props.bTheta);
+    return { x, y, distance }
+}
+
+const Box = ({ onWheel, color, bTheta, ...props }) => {
+
+    const [theta, setTheta] = useState(bTheta);
 
     useEffect(() => {
-        if (trigger) {
-            roll(trigger);
+        if (onWheel) {
+            roll(onWheel);
         }
-    }, [trigger]);
+    }, [onWheel]);
 
     const mesh = useRef()
 
     const roll = (e) => {
-        const { x, y: z, distance } = getCoordinates(theta)
-
         e.altKey ?
             setTheta((theta) => (theta - 2) % 360) :
             setTheta((theta) => (theta + 2) % 360)
-        // mesh.current.rotation.x = x + distance
-        mesh.current.rotation.y = x + distance
+
+        const { x, y: z } = getCoordinates(theta)
+
+        mesh.current.rotation.y = x / 2
         mesh.current.position.x = x
         mesh.current.position.z = z
-        if (color == 0) {
-            console.log({
-                theta: color,
-                rX: mesh.current.rotation.x,
-                rY: mesh.current.rotation.y,
-                rZ: mesh.current.rotation.z,
-            });
-        }
+        // if (color == 0) {
+        //     console.log({
+        //         theta: color,
+        //         rX: mesh.current.rotation.x,
+        //         rY: mesh.current.rotation.y,
+        //         rZ: mesh.current.rotation.z,
+        //     });
+        // }
     }
 
     return <>
-        {/* <Float
-            speed={1.5}
-            rotationIntensity={0.5}
-            floatingRange={[0, 0.01]}
-        > */}
+
         <mesh
             ref={mesh}
             {...props}
@@ -51,59 +52,24 @@ const Box = (props) => {
             <boxGeometry args={[2, 2.5, 1]} />
             <meshStandardMaterial metalness={0} roughness={0} color={`rgb(${color + 100},0,0)`} />
         </mesh>
-        {/* </Float> */}
     </>
 };
 
 
-const getCoordinates = (angle, distance = 3.5) => {
-    angle *= Math.PI / 180
-    let x = -distance * Math.cos(angle),
-        y = -distance * Math.sin(angle)
-
-    return { x, y, distance }
-}
-
-export default function Boxes({ count, trigger }) {
-
-    // const { position, target } = useControls('', {
-    //     position:
-    //     {
-    //         value: { x: 3, y: 5.5, z: 0.5 },
-    //         step: 0.01,
-    //         joystick: 'invertY'
-    //     },
-    //     target:
-    //     {
-    //         value: { x: 0.3, y: 0, z: -2.5 },
-    //         step: 0.01,
-    //         joystick: 'invertY'
-    //     }
-    // })
-
-    const spotLight = useRef()
-    // useHelper(spotLight, SpotLightHelper)
-    useEffect(() => {
-        // spotLight.current.target.updateMatrixWorld()
-    })
-
+export const Boxes = ({ count, onWheel }) => {
     const theta = 360 / count
 
     return <>
-
-        <ambientLight intensity={5} />
-
         {[...Array(count)].map((ref, i) => {
             let { x, y } = getCoordinates(i * theta)
 
             return <Box
-                trigger={trigger}
+                onWheel={onWheel}
                 bTheta={i * theta}
                 color={i * theta}
                 key={i}
                 position-x={x}
                 position-z={y}
-                // rotation-z={3.6 * i}
                 rotation-y={Math.PI * i}
                 scale={1}
             />
--- src/Experience.jsx
+++ src/Experience.jsx
@@ -2,11 +2,11 @@ import { OrbitControls } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber'
 import { Perf } from 'r3f-perf'
 import { useState } from 'react'
-import Boxes from './Boxes'
+import { Boxes } from './Boxes'
 
 
 export default function Experience() {
-    const [trigger, setTrigger] = useState();
+    const [wheelListener, setWheelListener] = useState();
 
     return <>
         <Canvas
@@ -14,17 +14,21 @@ export default function Experience() {
                 fov: 40,
                 near: 0.1,
                 far: 20,
+                // position: [0, 8, 5]
                 position: [0, 0, 2]
             }}
             onWheel={(e) => {
-                setTrigger(e);
+                setWheelListener(e);
             }}
         >
             {/* <OrbitControls /> */}
             {/* <Perf position='top-left' /> */}
+
+
+            <ambientLight intensity={5} />
             <axesHelper args={[2, 2, 2]} />
 
-            <Boxes trigger={trigger} count={5} />
+            <Boxes onWheel={wheelListener} count={5} />
         </Canvas>
     </>
 }
