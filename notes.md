98aae28de9e237dd1574f7e19ab6d81bce39465e
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2023-02-26 01:47:01 +0300

    some clean 🧹

---
+++ notes.md
@@ -0,0 +1,9 @@
+## camera
+
+            camera={{
+                fov: 40,
+                near: 0.1,
+                far: 100,
+                position: [0, 20, 5]
+                // position: [0, 0, 2]
+            }}
--- src/Experience.jsx
+++ src/Experience.jsx
@@ -2,7 +2,7 @@ import { OrbitControls } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber'
 import { Perf } from 'r3f-perf'
 import gsap from 'gsap';
-import { createRef, forwardRef, useEffect, useRef, useState } from 'react';
+import { createRef, useEffect, useRef, useState } from 'react';
 import { Box } from './Boxes'
 
 const getCoordinates = (angle, distance = 6) => {
@@ -13,10 +13,38 @@ const getCoordinates = (angle, distance = 6) => {
     return { x, y, distance }
 }
 
+const roll = (theta, ref) => {
+    const { x, y: z } = getCoordinates(theta)
+    gsap.to(
+        ref.current.rotation,
+        {
+            duration: 1.5,
+            ease: 'power2.inOut',
+            y: x / 2,
+        }
+    )
+    gsap.to(
+        ref.current.position,
+        {
+            duration: 1.5,
+            ease: 'power2.inOut',
+            x: x,
+            z: z
+        }
+    )
+}
+
 export default function Experience() {
-    const [wheelListener, setWheelListener] = useState();
-    const ref = useRef()
     const count = 5
+    const baseTheta = 360 / count
+    const boxesTheta = Array.from({ length: count }).map((r, i) => i * baseTheta)
+
+    const refs = useRef(
+        Array.from({ length: count }).map(() => createRef())
+    )
+
+    const [theta, setTheta] = useState(boxesTheta);
+
     // useEffect(() => {
     //     const handleClick = event => {
     //         console.log('Button clicked');
@@ -27,45 +55,9 @@ export default function Experience() {
     //     element.addEventListener('scroll', handleClick);
     // })
 
-    const baseTheta = 360 / count
-    const boxesTheta = Array.from({ length: count }).map((r, i) => i * baseTheta)
-
-    const refs = useRef(
-        Array.from({ length: count }).map(() => createRef())
-    )
-
-    const [theta, setTheta] = useState(boxesTheta);
-
-    const roll = (theta, ref) => {
-        const { x, y: z } = getCoordinates(theta)
-        gsap.to(
-            ref.current.rotation,
-            {
-                duration: 1.5,
-                ease: 'power2.inOut',
-                y: x / 2,
-            }
-        )
-        gsap.to(
-            ref.current.position,
-            {
-                duration: 1.5,
-                ease: 'power2.inOut',
-                x: x,
-                z: z
-            }
-        )
-    }
-
     return <>
         <Canvas
-            camera={{
-                fov: 40,
-                near: 0.1,
-                far: 100,
-                position: [0, 20, 5]
-                // position: [0, 0, 2]
-            }}
+            camera={{ position: [0, 20, 5] }}
             onWheel={(e) => {
                 setTheta((theta) => theta.map((t) => (t + 360 / 5) % 360))
                 refs.current.map((ref, i) => roll(theta[i], ref))
